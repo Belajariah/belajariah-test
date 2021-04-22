be36d1902bf950c8e718c51266098b5f05a75b30
Heri Heryanto <Heri.Heryanto-EXT@xapiens.id>
2021-04-22 09:16:49 +0700

    env

--- main.go
+++ main.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
+	_, err := os.Stat(".env")
 
-	fmt.Fprintf(w, "Hello boss!")
+	if !os.IsNotExist(err) {
+		err := godotenv.Load(".env")
+
+		if err != nil {
+			fmt.Println("Error while reading the env file", err)
+			panic(err)
+		}
+	}
+
+	title := os.Getenv("TITLE")
+	fmt.Fprintf(w, title)
 
 }
 
 func main() {
 	http.HandleFunc("/api", handleFunc)
+
 	log.Println("Server is running:", 3001)
 	log.Fatal(http.ListenAndServe(":3001", nil))
 }
